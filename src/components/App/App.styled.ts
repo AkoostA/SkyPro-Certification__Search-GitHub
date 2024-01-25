435f719349c75b4be951d4aa10f150a9dd1fe247
AkoostA <123000618+AkoostA@users.noreply.github.com>
2024-01-25 13:11:35 +0300

    Начальные настройки 1. Создал проект на ts с помощью react-app. 2. Удалил лишние файлы и предал начальную структуру для проекта. 3. Установил styled-component и переписал начальные стили.

---
+++ src/components/App/App.styled.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const DivTest = styled.div`
+  text-align: center;
+  color: white;
+`;
---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import { DivTest } from "./App.styled";
+
+function App() {
+  return <DivTest>Настройка проекта</DivTest>;
+}
+
+export default App;
