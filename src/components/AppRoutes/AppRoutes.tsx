fb53c71bde87ecb4f1f38a347cbe61f2099b0b24
AkoostA <123000618+AkoostA@users.noreply.github.com>
2024-01-29 11:15:51 +0300

    HashRouter

--- src/components/AppRoutes/AppRoutes.tsx
+++ src/components/AppRoutes/AppRoutes.tsx
@@ -4,11 +4,9 @@ import { Route, Routes } from "react-router-dom";
 import NotFound from "../../pages/NotFound/NotFound";
 
 function AppRoutes() {
-  const PATH = "/skypro__search-github";
-
   return (
     <Routes>
-      <Route path={PATH} element={<Main />} />
+      <Route path="/" element={<Main />} />
       <Route path="/user" element={<User />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
